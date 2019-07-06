4f611db57e1dbe8899b5a60f5fa4a2cc4d49c0d3
fewoot <50654807+fewoot@users.noreply.github.com>
2019-07-06 15:03:44 +0800

    1

--- others/Screen saver.c
+++ others/Screen saver.c
@@ -6,54 +6,7 @@
 
 int main()
 {
-    // printf("what the fuck?");
-    // system("pause");
-    // system("cls");
-    // system("pause");
-    //scanf("",&);
-    //gets();
-    //fflush(stdin);
-    printf("System in boot:");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".\n");
-    Sleep(1000);
-    printf("System boot complete\n");
-    Sleep(500);
-    printf("System version: v2.3422\n");
-    printf("System support: F-W-T v1.3342   @Ewit: OK   @Thihtu: FAILED\n");
-    printf("System status:\n");
-    Sleep(400);
-    printf("Starting Check: OK\n");
-    Sleep(300);
-    printf("Passive Check:  OK\n");
-    Sleep(200);
-    printf("Servo Library:  OK\n");
-    Sleep(600);
-    printf("Scan Serve:     FAILED\n");
-    Sleep(300);
-    printf("Security Blockade Protection: OK\n");
-    printf("Encryption services: OK\n");
-    Sleep(400);
-
-    printf("In network connection");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".");
-    Sleep(1000);
-    printf(".\n");
-    Sleep(1000);
-    printf("The network is connected\n");
-    //system("pause");
-    scanf("");
-    show();
-    //system("pause");
+    //show_screen();
     // printf("");
     // printf("");
     // printf("");
@@ -77,9 +30,77 @@ int main()
     return 0;
 }
 
+void qianqi(){
+    // printf("what the fuck?");
+    // system("pause");
+    // system("cls");
+    // system("pause");
+    //scanf("",&);
+    //gets();
+    //fflush(stdin);
+    printf("System in boot  ");
+    Sleep(1000);
+    printf(".");
+    Sleep(1000);
+    printf(".\n");
+    Sleep(1000);
+    printf("System boot complete\n");
+    Sleep(500);
+    printf("System version: v2.3422\n");
+    printf("System support: F-W-T v1.3342   @Ewit: OK   @Thihtu: FAILED\n");
+    printf("System status:\n");
+    Sleep(400);
+    printf("Starting Check: OK\n");
+    Sleep(300);
+    printf("Passive Check:  OK\n");
+    Sleep(200);
+    printf("Servo Library:  OK\n");
+    Sleep(600);
+    printf("Scan Serve:     FAILED\n");
+    Sleep(300);
+    printf("Security Blockade Protection: OK\n");
+    printf("Encryption services: OK\n");
+    Sleep(400);
+
+    printf("In network connection  ");
+    Sleep(1000);
+    printf(".");
+    Sleep(1000);
+    printf(".");
+    Sleep(1000);
+    printf(".");
+    Sleep(1000);
+    printf(".");
+    Sleep(1000);
+    printf(".\n");
+    Sleep(1000);
+    printf("The network is connected\n");
+    system("pause");
+    //scanf("");
+    show();
+    system("pause");
+}
+
 void show(){
-    for(int i=0;i<31;++i){
-        printf("%d%d%d%d%d%d\n",(rand() % (9-0))+ 0,(rand() % (9-0))+ 0,(rand() % (9-0))+ 0,(rand() % (9-0))+ 0,(rand() % (9-0))+ 0,(rand() % (9-0))+ 0);
-        sleep(600);
+    for(int i=0;i<600;++i){
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d\n",(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9));
+        Sleep(50);
+    }
+}
+
+void show_screen(){
+    for(int i=0;i<600;++i){
+        printf("%d %d %d %d %d %d %d ",(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9));
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d ",(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9));
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d ",(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9));
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d ",(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9));
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d ",(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9));
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d ",(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9));
+        printf("%d %d %d %d %d %d %d %d %d %d %d %d\n",(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9),(rand()%9));
+        Sleep(10);
+        if(i>300){
+            system("cls");
+            i = 0;
+        }
     }
 }
